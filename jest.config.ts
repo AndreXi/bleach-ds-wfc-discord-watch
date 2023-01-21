eec27dbcd2b078d67bc65c19964bac4931662970
AndreXi <adprodrigues@hotmail.com>
2023-01-20 20:08:14 -0400

    fix: rename

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/"],
   verbose: true,
   silent: true,
 };
