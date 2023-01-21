5aeff8b9a5259b8735e140c6a213a1c32a9cb446
AndreXi <adprodrigues@hotmail.com>
2023-01-20 20:02:47 -0400

    feat: add tests

--- src/data/utils.test.ts
+++ src/data/utils.test.ts
@@ -1,27 +1,106 @@
-import { getActiveGame } from "./utils";
+import { getActiveGame, getTrStrings, getActiveGames } from "./utils";
 
-// const html0 = `<html><head></head><body><table border="1">
-// <tbody><tr>
-//     <td>Game ID</td><td># Players</td>
-// </tr>
-// <tr>
-//     <td>bleach2USds</td>
-//     <td><center>1</center></td>
-// </tr>
-// <tr>
-//     <td>bleach2ds</td>
-//     <td><center>1</center></td>
-// </tr></tbody></table>
-// <br>
-// <i>Last updated: 2023-01-20 21:55:34.304967</i><br>
-// </body></html>`;
+const html0 = `<html><head></head><body><table border="1">
+<tbody><tr>
+    <td>Game ID</td><td># Players</td>
+</tr>
+<tr>
+    <td>bleach2USds</td>
+    <td><center>1</center></td>
+</tr>
+<tr>
+    <td>bleach2ds</td>
+    <td><center>1</center></td>
+</tr></tbody></table>
+<br>
+<i>Last updated: 2023-01-20 21:55:34.304967</i><br>
+</body></html>`;
 
-const trString0 = "\n            bleach2USds\n            1\n        ";
+const htmlEmpty = `<html><head></head><body><table border="1">
+<tbody><tr>
+    <td>Game ID</td><td># Players</td>
+</tr></tbody></table>
+<br>
+<i>Last updated: 2023-01-20 22:33:57.403785</i><br>
+</body></html>`;
 
-test("Check if can get Bleach 2 with 1 player", () => {
-  expect(getActiveGame(trString0)).toMatchObject({
+const htmlBleach2Only = `
+<html>
+    <table border='1'>
+        <tr>
+            <td>Game ID</td><td># Players</td>
+        </tr>
+        <tr>
+            <td>bleach2USds</td>
+            <td><center>1</center></td>
+        </tr></table>
+    <br>
+    <i>Last updated: 2023-01-20 19:55:07.609420</i><br>
+</html>`;
+
+const htmlBleach1Only = `<html><head></head><body><table border="1">
+<tbody><tr>
+    <td>Game ID</td><td># Players</td>
+</tr>
+<tr>
+    <td>bleach1USds</td>
+    <td><center>1</center></td>
+</tr></tbody></table>
+<br>
+<i>Last updated: 2023-01-20 23:42:40.170214</i><br>
+</body></html>`;
+
+const trStringB2USA = "\n            bleach2USds\n            1\n        ";
+
+test("Get nothing if there are no active games", () => {
+  expect(getActiveGames(htmlEmpty)).toStrictEqual(new Set());
+});
+
+test("Get Bleach 2 with 1 player", () => {
+  expect(getActiveGame(trStringB2USA)).toMatchObject({
     codename: "bleach2USds",
     players: 1,
-    name: "Bleach: Dark Souls DS [USA]"
-  })
+    name: "Bleach: Dark Souls DS [USA]",
+  });
+});
+
+test("Get Set of Bleach 2 with 1 player", () => {
+  expect(getActiveGames(htmlBleach2Only)).toStrictEqual(
+    new Set([
+      {
+        codename: "bleach2USds",
+        players: 1,
+        name: "Bleach: Dark Souls DS [USA]",
+      },
+    ])
+  );
+});
+
+test("Get Set of Bleach 2 USA and JPN with 1 player", () => {
+  expect(getActiveGames(html0)).toStrictEqual(
+    new Set([
+      {
+        codename: "bleach2USds",
+        players: 1,
+        name: "Bleach: Dark Souls DS [USA]",
+      },
+      {
+        codename: "bleach2ds",
+        players: 1,
+        name: "Bleach: Dark Souls DS [JPN]",
+      },
+    ])
+  );
+});
+
+test("Get Set of Bleach 1 USA with 1 player", () => {
+  expect(getActiveGames(htmlBleach1Only)).toStrictEqual(
+    new Set([
+      {
+        codename: "bleach1USds",
+        players: 1,
+        name: "Bleach: The Blade of Fate DS [USA]",
+      },
+    ])
+  );
 });
--- src/data/utils.ts
+++ src/data/utils.ts
@@ -1,4 +1,5 @@
 import gamecodes from "./gamecodes.json";
+import { JSDOM } from "jsdom";
 
 type ActiveGame = {
   codename: string;
@@ -8,12 +9,32 @@ type ActiveGame = {
 
 const gameNames = gamecodes as Record<string, string>;
 
+function getTrStrings(html: string): string[] {
+  // Parse HTML to easy pick the table rows
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+  const activePlayersTable = document.querySelectorAll("tr");
+  const trStrings: string[] = [];
+
+  activePlayersTable.forEach((element) => {
+    // trString = '\n            bleach2USds\n            1\n        '
+    const trString = element.textContent;
+
+    // Filter for "bleach" the common word in game codenames.
+    if (trString?.includes("bleach")) {
+      trStrings.push(trString);
+    }
+  });
+
+  return trStrings;
+}
+
 function getActiveGame(trString: string): ActiveGame | null {
   // trString is '\n            bleach2USds\n            1\n        '
   // Applying .match to get 'bleach2USds\n            1'
   const valueString = trString.match(/\w*\n\s+\d+/g)?.toString();
 
-  // Then, we split the value to codename, players
+  // Then, we split the value to [codename, players]
   const value = valueString?.split(/\n\s+/);
 
   if (value && gameNames[value[0]]) {
@@ -27,4 +48,16 @@ function getActiveGame(trString: string): ActiveGame | null {
   return null;
 }
 
-export { getActiveGame };
+function getActiveGames(html: string) {
+  const trStrings = getTrStrings(html);
+  const activeGames = new Set<ActiveGame>();
+
+  trStrings.forEach((trString) => {
+    const activeGame = getActiveGame(trString);
+    activeGame && activeGames.add(activeGame);
+  });
+
+  return activeGames;
+}
+
+export { getActiveGame, getTrStrings, getActiveGames };
